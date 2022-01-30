f95eb7f8ebbafff949339ec380c9b7866247de02
Christian Cordero <christian.cordero@insteclrg.edu.ec>
2022-01-30 18:23:39 -0500

    Init: Se crea la clase main operaciones

---
+++ src/main/java/com/ZonaInfo/operaciones/App.java
@@ -0,0 +1,13 @@
+package com.ZonaInfo.operaciones;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
